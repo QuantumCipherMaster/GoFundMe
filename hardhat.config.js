5daff68543e90ef2ee82a600ced64d25595048e3
QuantumCipherMaster <rogernigel60@gmail.com>
2025-03-27 17:53:33 -0600

    fix typo

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,6 +6,10 @@ const SEPOLIA_URL = process.env.SEPOLIA_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
+console.log("SEPOLIA_URL:", SEPOLIA_URL ? "Unset" : "Set");
+console.log("PRIVATE_KEY:", PRIVATE_KEY ? "Unset" : "Set");
+console.log("ETHERSCAN_API_KEY:", ETHERSCAN_API_KEY ? "Unset" : "Set");
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.20",
@@ -13,7 +17,6 @@ module.exports = {
     sepolia: {
       url: SEPOLIA_URL,
       accounts: [PRIVATE_KEY],
-      chianid: 11155111,
     },
   },
   etherscan: {
